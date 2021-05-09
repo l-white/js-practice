7157de2d9a265cc79466dcbeb8544cba0382eb19
Laura <law7676@outlook.com>
2021-05-09 10:01:15 -0500

    Create justCoolStuff.js

---
+++ justCoolStuff.js
@@ -0,0 +1,11 @@
+const justCoolStuff = (arr1, arr2) => {
+  const results = arr1.filter((word) => arr2.includes(word));
+  return results;
+};
+
+// Test
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff));
